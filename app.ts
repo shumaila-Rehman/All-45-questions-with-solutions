25aeb3d26698caf6291f4b01b3c2e4a7b6edcc38
Shumaila-Rehman <pathan369khan@gmail.com>
2024-03-24 19:22:39 +0500

    Add question #1

---
+++ app.ts
@@ -0,0 +1,11 @@
+//                     ========> Question # 1 <=========Personal Message: 
+//Store a person’s name in a variable, and print a message to that person. 
+//Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+
+
+let personName = " Eric";
+let message = `"Hello ${personName}, would you like to learn some Typescript today?"`;
+console.log(message);
+
+
